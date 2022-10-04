711c382bfb1937af8bd871b2adffefc1c51d201b
Jessey Angel <103680584+jesseyangel@users.noreply.github.com>
2022-10-03 22:20:55 -0300

    update html in js

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # TabelasEListasEmJS
  Criando tabelas com JS
+ Primeiro criamos tabelas responsivas com html estaticas e depois vamos aprender replicar essas tableas em JS.
+
+ Tentei deixar o codigo mais clean com HTML no JS, criei algumas funções que deiixa mais dinamico, só ajustando pra conseguir fazer funcionar 100%.
---
+++ scripts.js
@@ -0,0 +1,33 @@
+var rangel = {nome:"Rangel", vitorias: 2, empates: 1, derrotas: 1, pontos: 0};
+var gabriela = {nome:"Gabriela", vitorias: 1, empates: 1, derrotas: 2, pontos: 0};
+
+function calculaPontos(jogador) {
+    var pontos = (jogador.vitorias * 3) + jogador.empates;
+    return pontos;
+}
+
+rangel.pontos = calculaPontos(rangel);
+gabriela.pontos = calculaPontos(gabriela);
+
+var jogadores = [rangel, gabriela];
+
+function exibeJogadoresNaTela(jogadores){
+    var elemento = "";
+    for (var i = 0; i < jogadores.length; i++) {
+    elemento += "<tr><td>" + jogadores[i].nome + "</td>";
+    elemento += "<td>" + jogadores[i].vitorias + "</td>";
+    elemento += "<td>" + jogadores[i].empates + "</td>";
+    elemento += "<td>" + jogadores[i].derrotas + "</td>";
+    elemento += "<td>" + jogadores[i].pontos + "</td>";
+    elemento += "<td><button onClick='adicionarVitoria()'>Vitória</button></td>";
+    elemento += "<td><button onClick='adicionarEmpate()'>Empate</button></td>";
+    elemento += "<td><button onClick='adicionarDerrota()'>Derrota</button></td>";
+    elemento += "</tr>";
+
+    }
+
+    var tabelaJogadores = document.getElementById("tabelaJogadores");
+    tabelaJogadores.innerHTML = elemento;
+}
+
+exibeJogadoresNaTela(jogadores);
